961e767dfa5997f52597b57d3495e696e11b7ada
Vincent Stehlé <v-stehle@ti.com>
2012-09-12 18:56:14 +0200

    Hack! Print reloc_off for debug
    Signed-off-by: Vincent Stehlé <v-stehle@ti.com>

--- arch/arm/lib/board.c
+++ arch/arm/lib/board.c
@@ -463,7 +463,7 @@ void board_init_f(ulong bootflag)
 	gd->relocaddr = addr;
 	gd->start_addr_sp = addr_sp;
 	gd->reloc_off = addr - _TEXT_BASE;
-	debug("relocation Offset is: %08lx\n", gd->reloc_off);
+	printf("relocation Offset is: %08lx\n", gd->reloc_off);
 	if (new_fdt) {
 		memcpy(new_fdt, gd->fdt_blob, fdt_size);
 		gd->fdt_blob = new_fdt;
