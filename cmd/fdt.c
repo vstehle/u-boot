583e32de47351f0d196c7ec8588b146716fda8a9
Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
2015-12-17 12:11:09 +0900

    cmd_fdt: Fix fdt address information after the movement
    This patch fixes the address information of fdt.
    
    wrong case:
     => fdt addr 0x48000000
     => fdt move 0x48000000 0x41000000 0xa000
     => fdt addr
     The address of the fdt is 48000000
    
    Active address in this case is 0x41000000.
    
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

--- cmd/fdt.c
+++ cmd/fdt.c
@@ -238,6 +238,7 @@ static int do_fdt(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 			return 1;
 		}
 		set_working_fdt_addr((ulong)newaddr);
+		env_set_hex("fdtaddr", (ulong)working_fdt);
 #ifdef CONFIG_OF_SYSTEM_SETUP
 	/* Call the board-specific fixup routine */
 	} else if (strncmp(argv[1], "sys", 3) == 0) {
