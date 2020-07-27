d716d3be3f532c6b6c89e16f032d41c25e13755c
Hien Huynh <hien.huynh.px@renesas.com>
2020-07-27 10:41:19 +0700

    arm: mach-rmobile: correct cpu info for RZ/G2M v3.0
    This commit corrects cpu info for RZ/G2M v3.0 (aka r8a774a3).
    
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

--- arch/arm/mach-rmobile/cpu_info.c
+++ arch/arm/mach-rmobile/cpu_info.c
@@ -89,6 +89,12 @@ static int rmobile_cpuinfo_idx(void)
 			printf("CPU: Renesas Electronics R8A774A1 rev 1.1/rev 1.2\n");
 			return 0;
 		}
+
+		if ((rmobile_get_cpu_rev_integer() == 3) &&
+		    (rmobile_get_cpu_rev_fraction() == 0)) {
+			printf("CPU: Renesas Electronics R8A774A3\n");
+			return 0;
+		}
 	}
 
 	for (; i < ARRAY_SIZE(rmobile_cpuinfo); i++)
