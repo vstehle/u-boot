12d18799d3e1f3e83658531ddab0f802dd47115a
Peng Fan <peng.fan at nxp.com>
2020-09-16 14:56:55 +0200

    usb: ehci-mx6: Add i.MX8 OTG controller support
    The i.MX8 has two USB controllers: USBOH and USB3. The USBOH reuses
    previous i.MX6/7. It has same PHY IP as i.MX7ULP but NC registers
    are same as i.MX7D. So add its support in ehci-mx6 driver.
    
    Signed-off-by: Ye Li <ye.li at nxp.com>
    Signed-off-by: Peng Fan <peng.fan at nxp.com>

--- arch/arm/include/asm/arch-imx8/clock.h
+++ arch/arm/include/asm/arch-imx8/clock.h
@@ -24,4 +24,12 @@ enum mxc_clock {
 
 u32 mxc_get_clock(enum mxc_clock clk);
 
+#define PLL_USB_EN_USB_CLKS_MASK	(1 << 6)
+#define PLL_USB_PWR_MASK		(1 << 12)
+#define PLL_USB_ENABLE_MASK		(1 << 13)
+#define PLL_USB_BYPASS_MASK		(1 << 16)
+#define PLL_USB_REG_ENABLE_MASK		(1 << 21)
+#define PLL_USB_DIV_SEL_MASK		(7 << 22)
+#define PLL_USB_LOCK_MASK		(1 << 31)
+
 #endif /* __ASM_ARCH_IMX8_CLOCK_H__ */
